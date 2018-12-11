37433432ff8f779b7979841ff31868d31b94a6b3
Andrew <andrew@zenoni.com>
2018-12-10 17:40:21 -0800

    added my own program

---
+++ my_own_program.c
@@ -0,0 +1,26 @@
+/*
+Class: CPSC 346-02
+Team Member 1: Andrew Zenoni
+Team Member 2: N/A 
+GU Username of project lead: azenoni
+Pgm Name: proj2.c
+Pgm Desc: Asks the user for input and reuturns the number of words they inputted
+Usage: ./a.out
+*/
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+ 
+ for(int i = 0; i < 10; i++) {
+     printf("Printing out the number: %i", i);
+ }
+ 
+ return 0;
+}

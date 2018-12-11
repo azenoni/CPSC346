998c8ee05c7af1dcaea0a9f0b1329c8f66018de3
Andrew <andrew@zenoni.com>
2018-12-10 17:42:48 -0800

    changes

--- my_own_program.c
+++ my_own_program.c
@@ -1,14 +1,3 @@
-/*
-Class: CPSC 346-02
-Team Member 1: Andrew Zenoni
-Team Member 2: N/A 
-GU Username of project lead: azenoni
-Pgm Name: proj2.c
-Pgm Desc: Asks the user for input and reuturns the number of words they inputted
-Usage: ./a.out
-*/
-
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
